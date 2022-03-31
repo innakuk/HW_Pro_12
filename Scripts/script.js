54b318b4072dd6f583bfcaf652c2e87d70a3ca40
innakuk <kukharinna@gmail.com>
2022-03-31 23:05:39 +0300

    ДЗ 12. Условное ветвление V4

---
+++ Scripts/script.js
@@ -0,0 +1,65 @@
+// 1.
+// let x = 10;
+// let y = 7;
+//
+// let result = x > y ? 'x больше, чем y' : 'x не больше, чем y';
+// alert(result);
+
+
+// 2.
+// const num = prompt('Введите число');
+// const result = num % 2;
+// if (result === 0) {
+//     alert(`Число ${num} четное`);
+// } else {
+//     alert(`Число ${num} нечетное`);
+// }
+
+
+// 3.
+// const num = prompt('Введите целое число');
+// let numLengthResult;
+// switch (num.length) {
+//     case 1:
+//         numLengthResult = 'однозначное';
+//         break;
+//     case 2:
+//         numLengthResult = 'двухзначное';
+//         break;
+//     case 3:
+//         numLengthResult = 'трехзначное';
+//         break;
+//     default:
+//         numLengthResult = 'более чем трехзначное';
+// }
+//
+// let numSign = +num > 0 ? 'положительное' :
+//     +num < 0 ? 'отрицательное' :
+//         '';
+//
+//  alert(`Число ${num} ${numLengthResult} ${numSign}`);
+
+
+ // 4.
+// const firstNumber = +prompt('Введите первое число');
+// const secondNumber = +prompt('Введите второе число');
+// const thirdNumber = +prompt('Введите третье число');
+//
+// if (firstNumber >= secondNumber && firstNumber >= thirdNumber) {
+//     alert(`${firstNumber}`);
+// } else if (secondNumber >= firstNumber && secondNumber >= thirdNumber) {
+//     alert(`${secondNumber}`);
+// } else if (thirdNumber >= firstNumber && thirdNumber >= secondNumber) {
+//     alert(`${thirdNumber}`);
+// }
+
+//5.
+// const firstLength = +prompt('Введите длину первой стороны треугольника');
+// const secondLength = +prompt('Введите длину второй стороны треугольника');
+// const thirdLength = +prompt('Введите длину третьей стороны треугольника');
+//
+// if ((firstLength + secondLength) < thirdLength || (firstLength + thirdLength) < secondLength || (thirdLength + secondLength) < firstLength) {
+//     alert('Треугольник не может существовать')
+// } else {
+//     alert('Треугольник может существовать')
+// }
